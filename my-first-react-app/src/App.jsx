6f4d8015fe0846cf7d4e0d1105a5646352d209fb
radhika kikani <radhikakikani123@gmail.com>
2025-07-10 21:11:28 +0530

    add app index and main file

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,8 +1,20 @@
+import { useState, useEffect } from "react"
+
 const Card=({title}
-)=>{
+     )=>{
+  const [count, setcount] = useState(0)    
+  const [hasLiked, sethasLiked ] = useState(false);
+
+  useEffect( ()=>{
+     console.log(`${title} has been liked: ${hasLiked}`)
+  }) 
+
    return(
-    <div>
+    <div className="card" onClick={()=> setcount((prevState)=>prevState+1)}>
       <h2>{title}</h2>
+      <button onClick={() => sethasLiked((prevState)=>!prevState)}>
+       {hasLiked? '❤️': '🤍'}   
+      </button>
     </div>
     
    )           
@@ -10,10 +22,10 @@ const Card=({title}
 
  
 const App = ()=>{
+  
   return(
-    <div>
-      <h1>Functional Arrow Component </h1>
-ad     
+    <div className="card-container">
+      <h1>Functional Arrow Component </h1>  
        <Card title="Avatar" rating={5} actor="YZZ"/>
        <Card title="The Lion King"/>
        <Card title="Jasus"/>
