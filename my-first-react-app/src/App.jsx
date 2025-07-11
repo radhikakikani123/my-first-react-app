081df7602e5ec8095530abafbe7c87563f16e5b9
radhika kikani <radhikikani123@gmail.com>
2025-07-11 10:35:54 +0530

    delete all unnecessar button

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,36 +1,9 @@
-import { useState, useEffect } from "react"
+import React from 'react'
 
-const Card=({title}
-     )=>{
-  const [count, setcount] = useState(0)    
-  const [hasLiked, sethasLiked ] = useState(false);
-
-  useEffect( ()=>{
-     console.log(`${title} has been liked: ${hasLiked}`)
-  }) 
-
-   return(
-    <div className="card" onClick={()=> setcount((prevState)=>prevState+1)}>
-      <h2>{title}-{count}</h2>
-      <button onClick={() => sethasLiked((prevState)=>!prevState)}>
-       {hasLiked? '❤️': '🤍'}   
-      </button>
-    </div>
-    
-   )           
-}
-
- 
-const App = ()=>{
-  
-  return(
-    <div className="card-container">
-      <h1>Functional Arrow Component </h1>  
-       <Card title="Avatar" rating={5} actor="YZZ"/>
-       <Card title="The Lion King"/>
-       <Card title="Jasus"/>
-    </div>
-   
+const App = () => {
+  return (
+    <div>App</div>
   )
 }
+
 export default App
