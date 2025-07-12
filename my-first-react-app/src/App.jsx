07b35abf55ecce69ddc9caa20ab49428f6aeaac3
radhika kikani <radhikikani123@gmail.com>
2025-07-12 16:05:05 +0530

    add API key

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,7 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect,useState } from 'react'
 import Search from './components/search'
 const App = () => {
   const [searchTerm, setsearchTerm] = useState("")
+
+  const API_BASE_URL ='https://api.themoviedb.org/3';
+  
+  const API_KEY= import.meta.env.VITE_TMDB_API_KEY;
+
+  const API_OPTION={
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+
+  }
+
+  
+  useEffect(()=>{
+
+  },[])
+
   return (
     <main>
       <div className='pattern' />
@@ -12,7 +31,7 @@ const App = () => {
         <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without The Hussle</h1>
 
         </header>
-       <Search Search={searchTerm}/>
+       <Search setsearchTerm={setsearchTerm} />
         </div>
     </main>
     
